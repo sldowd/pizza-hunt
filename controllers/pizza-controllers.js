587187ca3935ec817a01347390dcf8e8b7074c85
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-03-18 18:44:36 -0400

    made comment model and controller and routes

--- controllers/pizza-controllers.js
+++ controllers/pizza-controllers.js
@@ -1,10 +1,16 @@
-const { Pizza } = require('../models/pizza');
+const { Pizza } = require('../models');
 
 const pizzaController = {
     // the functions will go here as methods
     // get all pizzas
     getAllPizza(req,res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -15,6 +21,11 @@ const pizzaController = {
     // get one pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbPizzaData => {
                 // if no pizza is found send 404
                 if(!dbPizzaData) {
--- models/pizza.js
+++ models/pizza.js
@@ -2,26 +2,45 @@
 const { Schema, model } = require('mongoose');
 
 // define pizza model -- not necessary but helps with code readability
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
-});
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+// get total count of comments and replies on retrieval 
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+})
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
 // export the Pizza model
-module.exports = { Pizza };
+module.exports = Pizza;
